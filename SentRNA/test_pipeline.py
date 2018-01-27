1d42fe0f8d48b3aec7e03818e3ce6178c00d2d1f
jadeshi <jadeshi@stanford.edu>
2018-01-26 16:15:43 -0800

    bugfix

--- SentRNA/test_pipeline.py
+++ SentRNA/test_pipeline.py
@@ -4,7 +4,7 @@ import pickle
 # Train test
 os.system('python run.py --mode train --input_data ../data/train/eterna_complete_ss.pkl --puzzle_names ../data/train/eterna_complete_ss.puzzle_names.pkl --puzzle_solution_count ../data/train/eterna_complete_ss.puzzle_solution_count.pkl --results_path model --n_long_range_features 20')
 # Test test
-os.system('python run.py --mode test --input_data ../data/test/eterna100.pkl --test_model model --results_path model_tested')
+os.system('python run.py --mode test --input_data ../data/test/eterna100.pkl --test_model model --results_path model_tested.pkl')
 # Refine test
 os.system('python run.py --mode refine --input_data test_results/model_tested.pkl --results_path model_refined.pkl')
 
