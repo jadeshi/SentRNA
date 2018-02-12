41d4024b62d6077b1cb9445d23398ad4cbce713a
jadeshi <jadeshi@stanford.edu>
2018-02-12 13:41:13 -0800

    updates

--- SentRNA/test_trained_model.py
+++ SentRNA/test_trained_model.py
@@ -2,15 +2,16 @@ import pickle
 import os
 
 ############ PASTE PARAMS HERE ######################
-path = '../models/trained_models/batch1/test/trial-14_MI-20'
-puzzle_name = '"1,2,3and4bulges"'
-renderer = 'rnaplot'
-moveset = "[GoodPairsMove(), MissingPairsMove()]"
+path = '../models/trained_models/matlot/test/MI-54_trial4'
+puzzle_name = 'Mat - Lot 2-2 B'
+renderer = 'eterna'
+moveset = "[GoodPairsMove([['G', 'C'], ['C', 'G']]), MissingPairsMove([['G', 'C'], ['C', 'G']])]"
 n_trajs = 300
+MI_tolerance = 1e-5
 ############ PASTE PARAMS HERE ######################
 
 
-os.system("python run.py --mode test --renderer %s --input_data ../data/test/eterna100.pkl --test_model %s --results_path out.pkl --test_puzzle_name '%s'"%(renderer, path, puzzle_name))
+os.system("python run.py --mode test --renderer %s --input_data ../data/test/eterna100.pkl --test_model %s --results_path out.pkl --test_puzzle_name '%s' --MI_tolerance %f"%(renderer, path, puzzle_name, MI_tolerance))
 
 for i in range(100):
     os.system("python run.py --mode refine --input_data test_results/out.pkl --results_path refined_%d.pkl --refine_puzzle_name '%s' --move_set \"%s\" --n_trajs %d"%(i, puzzle_name, moveset, n_trajs))
